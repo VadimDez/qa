95e23fa4001e1cb0e5b740bcb40fbe1e47fd4969
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2019-02-02 11:12:48 +0100

    add card

--- qa5-app/App.js
+++ qa5-app/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image
+} from 'react-native';
 
 export default class App extends React.Component {
+  onPress() {
+
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={ styles.container }>
+        <View style={ styles.checkContainer }>
+          <Text>Title</Text>
+          <Image
+           style={ styles.instructions }
+           source={require('./resources/002.jpg')}
+          />
+          <View>
+            <Button onPress={ this.onPress }
+                    title="Next"
+                    color="#000"
+            />
+            <Button onPress={ this.onPress }
+                    title="Next"
+                    color="#000"
+            />
+          </View>
+        </View>
       </View>
     );
   }
@@ -18,4 +43,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  checkContainer: {
+    flex: 1,
+    padding: 40,
+    backgroundColor: 'red',
+    width: '100%'
+  },
+  title: {},
+  instructions: {
+    flex: 1,
+    width: '100%'
+  }
 });
